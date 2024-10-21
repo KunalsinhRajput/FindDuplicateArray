9d99af8d2411b249acfb22fd5fc49381074f8711
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-10-21 12:17:37 +0530

    Add project files.

---
+++ FindDuplicateArray/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDuplicateArray
+{
+    class Class1
+
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "java","C","C" ,"C#","Python" };
+            
+            Boolean flag = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine("Duplicate value is " + arr[i]);
+                        
+                        flag = true;
+                      
+                   
+                    }
+                }
+               
+            }
+            if (flag == false)
+            {
+                Console.WriteLine("Duplicate value is not found");
+            }
+
+        }
+
+        
+
+        
+    }
+}
